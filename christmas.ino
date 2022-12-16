5f6c961ccaa5346f00864a3199fd375187e764a0
vanten-s <svante.spannar@gmail.com>
2022-12-16 11:37:56 +0100

    More modes

--- christmas.ino
+++ christmas.ino
@@ -6,13 +6,16 @@
 #define N_LEDS 300
 
 // Define number of modes
-#define N_MODES 1
+#define N_MODES 2
 
 // Led array
 CRGB leds[ N_LEDS ];
 
 // Time as a uint8_t to make it much easier to use with CHSV( ) 
 uint8_t time = 0;
+
+// Color for color specific operations
+CRGB color = CHSV(212, 255, 255);
 
 // What mode are we using right now
 int mode = 0;
@@ -33,8 +36,23 @@ int rainbow_solid ( )
 
 }
 
+// Mode for making the tree go crazy
+int blink ( )
+{
+
+    // Set every LED to a random hue
+    for ( int i = 0; i < N_LEDS; i++ )
+    {
+        leds[i] = CHSV(random8(), 255, 255);
+    }
+
+    // Succes
+    return 1;
+
+}
+
 // Define array of int functions
-int ( *modes[ N_MODES ] )( ) = { rainbow_solid };
+int ( *modes[ N_MODES ] )( ) = { rainbow_solid, blink };
 
 // Run on start
 void setup ( )
